fd9ea4000626d48ff3e0ccc133690441df51cd9a
FloMoro <flora.moreau@mmibordeaux.com>
2021-01-29 18:43:00 +0100

    reglage prob js scroll

--- js.js
+++ js.js
@@ -1,13 +1,14 @@
 const container = document.getElementsByClassName('btna');
 const html = document.querySelector('html');
 
-const closeBtn = document.querySelector('.closebtn');
+const closeBtn = document.querySelectorAll('.closebtn');
 
 for (let i = 0; i<container.length; i++){
 container[i].addEventListener('click', ()=>{
     html.classList.toggle('active');
 })}
 
-closeBtn.addEventListener('click', ()=>{
+for (let i = 0; i<closeBtn.length; i++){
+closeBtn[i].addEventListener('click', ()=>{
     html.classList.toggle('active');
-})
+})}
